18fbef75597cdf71ce16c0657e80a442a619341b
Jocelyn <jocelyn.kerbourch@gmail.com>
2017-12-23 11:23:23 +0100

    set conversation

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
-# gaia
+# Gaia
 
-Wellness Bot for Heores.
+#### Wellness Bot for Heores.
 
 ## Commands
 
+ - Ask 5 questions and give you advice
 ```
-> Give me a tips
-// Ask 5 questions and give you advice
-
-> Add question
-// Add a question
-
+ > Give me a tips
+```
+ - Add a question
+```
+ > Add question
+```
+- Add a tips
+```
 > Add tips
-// Add a tips
-
->> Give me stats
-// Displays the usage stats of the bot
+```
+- Displays the usage stats of the bot
+```
+> Give me stats
 ```
 
+
+## Develpement
+
+- DynamoDB Schema
+
+![DynamoDB Schema](https://s3.eu-west-3.amazonaws.com/botgaia/github/schema_db.png "DynamoDB Schema")
+
+- Lambda
+
+[Lambda Best Practices](http://docs.aws.amazon.com/en_en/lambda/latest/dg/best-practices.html)
---
+++ src/set_conversation.js
@@ -0,0 +1,47 @@
+var AWS = require('aws-sdk');
+
+exports.handler = function(event, context, callback) {
+    setConversation (callback);
+}
+ 
+function setConversation (callback) {
+
+    var uniqid = (new Date().getTime() + Math.floor((Math.random()*10000)+1)).toString(16);
+    var created_at = new Date().toISOString();
+    var status = "pending";
+    var items = {
+            "id": uniqid,
+            "created_at": created_at,
+            "status": status
+        };
+    var params = {
+        TableName:"gaia_conversations",
+        Item:items
+    };
+
+    putItem(params, getMessage, callback);
+}
+
+function putItem(params, getMessage, callback) {
+    var documentClient = new AWS.DynamoDB.DocumentClient();
+    documentClient.put(params, function(err, data) {
+        var status = "insert";
+        if (err) {
+            status = "error";
+        }
+
+        var result = getMessage(status,params.Item['id']);
+        callback(null, result);
+    });
+}
+
+function getMessage(status,uniqid){
+    var body = {"status": status, "id":uniqid};
+    var statusCode = 200;
+    return {
+        "statusCode": statusCode,
+        "headers": {},
+        "body": JSON.stringify(body)
+    };
+}
+
