f7261f27bd34d2743b738d9515e204a5991a73ee
Jocelyn <jocelyn.kerbourch@gmail.com>
2017-12-26 07:45:15 +0100

    Informations fonctions : questions, tips, features

---
+++ src/get_list_questions.js
@@ -0,0 +1,32 @@
+var AWS = require('aws-sdk');
+var tools = require('./tools.js');
+
+exports.handler = function(event, context, callback) {
+    getQuestions (event, callback);
+}
+ 
+function getQuestions (event, callback) {
+
+    var conversation = tools.getParameter(event,"conversation");
+    var params = {
+        TableName: "gaia_questions"
+    };
+
+    var documentClient = new AWS.DynamoDB.DocumentClient();
+    documentClient.scan(params, function(err, data) {
+        
+        var status = "scan";
+        var infos={"params":params, "data":{}};
+        if (err) {
+            status = "error";
+        } else {
+            if (data.Items.length==0){
+                status = "empty";
+            }else{
+                infos['data']=data.Items;
+            }
+        }
+        var result = tools.getMessage(status,infos);
+        callback(null, result);
+    });
+}
